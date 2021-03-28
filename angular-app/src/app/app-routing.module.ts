47f4347287acbb3b835c3cefa845fcd58b6543f8
Michiel Stornebrink <michielstornebrink@gmail.com>
2021-03-28 20:28:51 +0200

    Move notification center to separate page

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { InstallerComponent } from "./admin/installer/installer.component";
 import { HomeComponent } from "./layout/home/home.component";
+import { NotificationCenterComponent } from "./notification-center/notification-center.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "prototype/welcome", component: HomeComponent },
   { path: "admin/installer", component: InstallerComponent },
+  { path: "notifications", component: NotificationCenterComponent },
 ];
 
 @NgModule({
