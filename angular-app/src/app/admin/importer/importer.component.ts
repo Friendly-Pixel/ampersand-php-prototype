037075f4b8dc78a7e32ccea23415753f0708c494
Michiel Stornebrink <michielstornebrink@gmail.com>
2021-03-30 22:14:52 +0200

    Add error handler to ImporterComponent

--- angular-app/src/app/admin/importer/importer.component.ts
+++ angular-app/src/app/admin/importer/importer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { FileUploader } from "ng2-file-upload";
+import { NavbarService } from "src/app/navbar/navbar.service";
+import { NotificationCenterService } from "src/app/notification-center/notification-center.service";
 
 @Component({
   selector: "app-importer",
@@ -12,10 +14,38 @@ export class ImporterComponent implements OnInit {
   hasAnotherDropZoneOver: boolean;
   response: string;
 
-  constructor() {
+  constructor(
+    protected navBar: NavbarService,
+    protected notifySvc: NotificationCenterService
+  ) {
     this.uploader = new FileUploader({
       url: "api/v1/admin/import",
     });
+
+    this.uploader.onSuccessItem = (fileItem, res, status, headers) => {
+      let response = JSON.parse(res);
+      this.notifySvc.updateNotifications(response.notifications);
+      if (response.sessionRefreshAdvice) this.navBar.refreshNavBar();
+    };
+
+    this.uploader.onErrorItem = (item, res, status, headers) => {
+      let response = JSON.parse(res);
+      let message: string;
+      let details: string;
+      if (typeof response === "object") {
+        if (response.notifications !== undefined) {
+          this.notifySvc.updateNotifications(response.notifications);
+        }
+        message = response.msg || "Error while importing";
+        this.notifySvc.error(message);
+        // .addError(message, status, true, response.html);
+      } else {
+        message = status + " Error while importing";
+        details = response; // html content is excepted
+        this.notifySvc.error(message);
+        // .addError(message, status, true, details);
+      }
+    };
 
     this.hasBaseDropZoneOver = false;
     this.response = "";
