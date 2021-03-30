2121d0853d89e712e5cb1cbef88f481e9ecedf00
Michiel Stornebrink <michielstornebrink@gmail.com>
2021-03-30 20:47:46 +0200

    Update HomeComponent from Material to Bootstrap

--- angular-app/src/app/layout/home/home.component.ts
+++ angular-app/src/app/layout/home/home.component.ts
@@ -6,28 +6,5 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  numberOfCols: number;
-  constructor() {}
-
-  ngOnInit(): void {
-    this.numberOfCols = this.getColsFor(window.innerWidth);
-  }
-
-  onResize(event) {
-    this.numberOfCols = this.getColsFor(event.target.innerWidth);
-  }
-
-  protected getColsFor(width: number): number {
-    if (width < 600) {
-      return 1;
-    }
-    if (width < 1024) {
-      return 2;
-    }
-    if (width < 1440) {
-      return 3;
-    }
-
-    return 4;
-  }
+  ngOnInit(): void {}
 }
