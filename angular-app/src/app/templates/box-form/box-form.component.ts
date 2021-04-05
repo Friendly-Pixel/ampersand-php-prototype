3da8427f078969a8eda162285eea429bbd76c313
Michiel Stornebrink <michielstornebrink@gmail.com>
2021-04-05 13:12:32 +0200

    Add BoxFormComponent

---
+++ angular-app/src/app/templates/box-form/box-form.component.ts
@@ -0,0 +1,47 @@
+import { Component, Input, OnInit } from "@angular/core";
+import { BoxFormItemComponent } from "../box-form-item/box-form-item.component";
+
+@Component({
+  selector: "app-box-form",
+  templateUrl: "./box-form.component.html",
+  styleUrls: ["./box-form.component.css"],
+})
+export class BoxFormComponent implements OnInit {
+  public crudC = false;
+  public crudR = true;
+  public crudU = false;
+  public crudD = false;
+
+  public showNavMenu = false;
+  public noRootTitle = true;
+  public title: string = "";
+  public hideOnNoRecords = false;
+
+  public isUni = true;
+  public isTot = false;
+
+  @Input()
+  public tgtConceptLabel: string;
+
+  @Input()
+  public data: Array<object> = [];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  showTitle(): boolean {
+    return !this.noRootTitle && this.title !== "";
+  }
+
+  isCrudU(): boolean {
+    return this.crudU;
+  }
+  isCrudD(): boolean {
+    return this.crudD;
+  }
+
+  createItem(): void {}
+  removeItem(item: BoxFormItemComponent): void {}
+  deleteItem(item: BoxFormItemComponent): void {}
+}
