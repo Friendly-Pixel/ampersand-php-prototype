e8606d5eb395632ef2f3f343b2877fabe163bb82
Michiel Stornebrink <michielstornebrink@gmail.com>
2023-07-12 20:36:38 +0200

    Add library project for atomic-box-view component
    Run ng generate library prototype-components

---
+++ frontend/projects/prototype-components/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of prototype-components
+ */
+
+export * from './lib/prototype-components.service';
+export * from './lib/prototype-components.component';
+export * from './lib/prototype-components.module';
